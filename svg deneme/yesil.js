97aea9a646c74592d74c586fd3610aadbe71d96a
Ufuk <ufuksipahi89@gmail.com>
2022-06-19 00:17:01 +0300

    yesilsi

--- svg deneme/yesil.js
+++ svg deneme/yesil.js
@@ -7,5 +7,13 @@ let a=harf[i].getTotalLength();
 if (i==0)
 harf[i].style.cssText=`stroke-dasharray: ${a}px; stroke-dashoffset: ${a}px; animation: line-anim 2s ease forwards 1s;`
 else
-harf[i].style.cssText=`stroke-dasharray: ${a}px; stroke-dashoffset: ${a}px; animation: line-anim 2s ease forwards ${i*2}s;`
+harf[i].style.cssText=`stroke-dasharray: ${a}px; stroke-dashoffset: ${a}px; animation: line-anim 2s ease forwards ${i*1.5}s;`
 }
+let b=0;
+setTimeout(myFunction,13000);
+function myFunction(){
+    if (b==0)
+        for(let i=0;i<harf.length;i++) harf[i].style.cssText=`stroke:white`;
+    b=1;
+   }
+
